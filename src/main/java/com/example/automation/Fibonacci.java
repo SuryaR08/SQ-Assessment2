3109f0f308e92b317298abd6eb62da772564753f
SuryaR08 <suryaraja8903@gmail.com>
2024-12-31 13:18:09 +0530

    modified

--- src/main/java/com/example/automation/Fibonacci.java
+++ src/main/java/com/example/automation/Fibonacci.java
@@ -28,4 +28,19 @@ public class Fibonacci {
         }
         return getNthFibonacci(n - 1) + getNthFibonacci(n - 2);
     }
+
+    public int[] generateSeriesDuplicated1(int n) {
+        if (n <= 0) {
+            return new int[0];
+        }
+        int[] series = new int[n];
+        series[0] = 0;
+        if (n > 1) {
+            series[1] = 1;
+            for (int i = 2; i < n; i++) {
+                series[i] = series[i - 1] + series[i - 2];
+            }
+        }
+        return series;
+    }
 }
