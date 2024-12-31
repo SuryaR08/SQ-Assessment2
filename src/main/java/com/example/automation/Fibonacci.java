f7eb4fe556d4fb840fecba7ef7a2127d46580814
SuryaR08 <suryaraja8903@gmail.com>
2024-12-31 13:10:07 +0530

    added test files

---
+++ src/main/java/com/example/automation/Fibonacci.java
@@ -0,0 +1,31 @@
+package com.example.automation;
+
+public class Fibonacci {
+    public int[] generateSeries(int n) {
+        if (n <= 0) {
+            return new int[0];
+        }
+        int[] series = new int[n];
+        series[0] = 0;
+        if (n > 1) {
+            series[1] = 1;
+            for (int i = 2; i < n; i++) {
+                series[i] = series[i - 1] + series[i - 2];
+            }
+        }
+        return series;
+    }
+
+    public int getNthFibonacci(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 0;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        return getNthFibonacci(n - 1) + getNthFibonacci(n - 2);
+    }
+}
