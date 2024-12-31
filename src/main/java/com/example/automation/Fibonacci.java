9708ddb3de3bda48c1dade0d2c5701d42feaa211
SuryaR08 <suryaraja8903@gmail.com>
2024-12-31 13:48:43 +0530

    modified

--- src/main/java/com/example/automation/Fibonacci.java
+++ src/main/java/com/example/automation/Fibonacci.java
@@ -5,8 +5,17 @@ public class Fibonacci {
         if (n <= 0) {
             return new int[0];
         }
+        if (n <= 0) {
+            return new int[0];
+        }
         int[] series = new int[n];
         series[0] = 0;
+        if (n > 1) {
+            series[1] = 1;
+            for (int i = 2; i < n; i++) {
+                series[i] = series[i - 1] + series[i - 2];
+            }
+        }
         if (n > 1) {
             series[1] = 1;
             for (int i = 2; i < n; i++) {
@@ -23,33 +32,25 @@ public class Fibonacci {
         if (n == 1) {
             return 0;
         }
+        if (n == 1) {
+            return 0;
+        }
+        if (n == 2) {
+            return 1;
+        }
         if (n == 2) {
             return 1;
         }
         return getNthFibonacci(n - 1) + getNthFibonacci(n - 2);
     }
 
-    public int[] generateSeriesDuplicated1(int n) {
-        if (n <= 0) {
-            return new int[0];
-        }
-        int[] series = new int[n];
-        series[0] = 0;
-        if (n > 1) {
-            series[1] = 1;
-            for (int i = 2; i < n; i++) {
-                series[i] = series[i - 1] + series[i - 2];
-            }
-        }
-        return series;
-    }
     public int duplicateFibonacci1(int n) {
-        if (n <= 1) return n;
+        if (n <= 1){
+            return n;
+        }
+        if (n <= 1){
+            return n;
+        }
         return duplicateFibonacci1(n - 1) + duplicateFibonacci1(n - 2);
-    }
-    
-    public int duplicateFibonacci2(int n) {
-        if (n <= 1) return n;
-        return duplicateFibonacci2(n - 1) + duplicateFibonacci2(n - 2);
     }
 }
